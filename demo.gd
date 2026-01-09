1db89b08d0dcf3b9e4670c81e03b43c8a5ed363f
kyradais <kyradais@gmail.com>
2026-01-09 17:03:39 +0800

    last update

--- demo.gd
+++ demo.gd
@@ -5,11 +5,8 @@ extends Node2D
 
 func _ready() -> void:
 	armature.debug = true
-	armature.current_animation = "bone_armtail_rotation"
+	armature.current_animation = "bone_root_rotation"
 
 func _process(delta: float) -> void:
-	var bone_root := armature.get_bone("bone_root")
-	var bone_tail := armature.get_bone("bone_tail")
-	
-	
-	icon_node.transform = bone_root.global_transform * bone_tail.transform
+	var slot := armature.global_position + armature.get_bone("bone_tail").position
+	print(armature.get_bone("bone_root").get_parent().get_name())
