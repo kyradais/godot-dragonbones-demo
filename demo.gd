c9c0567aa215058fd5675eb0105d811bc2af698f
kyradais <kyradais@gmail.com>
2026-01-09 11:22:13 +0800

    latest update

--- demo.gd
+++ demo.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 @onready var armature :DragonBonesArmatureView = $Object
+@onready var icon_node :Node2D = $Icon
 
 func _ready() -> void:
 	armature.debug = true
-	var a = armature.get_armature()
-	armature.current_animation	= "bone_armtail_rotation"
-	print("This is :", a)	
+	armature.current_animation = "bone_armtail_rotation"
 
 func _process(delta: float) -> void:
-	var a = armature.get_bone("bone_tail")
-	print("That is :", a.rotation)
+	var bone_root := armature.get_bone("bone_root")
+	var bone_tail := armature.get_bone("bone_tail")
+	
+	
+	icon_node.transform = bone_root.global_transform * bone_tail.transform
